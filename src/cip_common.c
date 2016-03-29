aac30bf21198fdac3679aebb16f09d3a6e2f869a
guodong <gd@tongjo.com>
2016-03-29 20:28:45 +0800

    c99

--- src/cip_common.c
+++ src/cip_common.c
@@ -23,7 +23,8 @@ char *rand_string(char *str, size_t size)
     const char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
     if (size) {
         --size;
-        for (size_t n = 0; n < size; n++) {
+        size_t n;
+        for (n = 0; n < size; n++) {
             int key = rand() % (int) (sizeof charset - 1);
             str[n] = charset[key];
         }
