ff3fc201be6784035d281aed3f869d9ebaa77e86
guodong <gd@tongjo.com>
2016-04-15 10:42:00 +0800

    add mouse event

--- src/cip_common.c
+++ src/cip_common.c
@@ -63,6 +63,10 @@ int get_size_by_type(enum CIP_EVENT type)
     switch (type) {
         case CIP_EVENT_MOUSE_MOVE:
             return sizeof(cip_event_mouse_move_t);
+        case CIP_EVENT_MOUSE_DOWN:
+            return sizeof(cip_event_mouse_down_t);
+        case CIP_EVENT_MOUSE_UP:
+            return sizeof(cip_event_mouse_up_t);
         case CIP_EVENT_KEY_DOWN:
             return sizeof(cip_event_key_down_t);
         case CIP_EVENT_KEY_UP:
